4fd6c5793c54dd3b18b1a545748959aafc2708ec
SoftWaren1 <aitareodas@outlook.com>
2024-01-09 09:06:45 +0530

    Update

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -15,7 +15,7 @@ public class Earthhack implements Globals
 {
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.0.3+alpha-35.2b1e947084";
+    public static final String VERSION = "1.0.0";
 
     public static void preInit()
     {
